70bd901a187683482af134f669c7d6bdd2c8b232
jrili <jfmrili@gmail.com>
2025-05-20 16:33:04 +0800

    Remove json data debug logging; Add method docstrings; shorten request sleep time to 500ms

--- get_all_election_results.py
+++ get_all_election_results.py
@@ -11,6 +11,15 @@ from src import config
 available_top_levels = ['local', 'overseas']
 
 def get_json(url):
+    """Get json data from URL
+
+    Keyword Arguments:
+    url -- url to get from
+
+    Return:
+    dict containing json data,
+    None if http status code is not success or if does not contain json data
+    """
     logging_utils.logger.debug(f"In get_json(): Getting from url: '{url}'")
     try: 
         response = requests.get(url)
@@ -21,7 +30,6 @@ def get_json(url):
             return None
 
         response_json = response.json()
-        logging_utils.logger.debug(f"response_json: {response_json}")
 
         logging_utils.logger.debug(f"In get_json(): Ended")
         sleep(config.REQUEST_SLEEP_S)
@@ -66,6 +74,16 @@ def get_region_er_json(top_level, src_category_code, code=0):
     return response_json
 
 def write_json(json_data:dict, path_to_file:str):
+    """ Write dict object to a json file
+
+    Keyword Arguments:
+    json_data -- dict containing json data
+    path_to_file -- relative path with respect to working directory upon running main script
+        Parent and child directories will be automatically created if not existing
+
+    Return:
+    None
+    """
     logging_utils.logger.debug(f"In write_json(): path_to_file:'{path_to_file}'")
 
     file_dir = os.path.dirname(path_to_file)
@@ -85,7 +103,7 @@ def get_output_path(parents:list, filename=None):
         ['local', 'R04A000', '3400000', '3403000', '3403008']
 
     Return:
-    String containing path relative to config.OUTPUT_PATH
+    String containing path including config.OUTPUT_PATH
     """
     output_path = config.OUTPUT_PATH
     for parent in parents:
@@ -96,117 +114,117 @@ def get_output_path(parents:list, filename=None):
 
     return output_path
 
-
 logging_utils.init_logger()
 logging_utils.logger.info("PH Election Results 2025 Scraper: Started.")
 
-# TOP-LEVEL: local, overseas
-for top_level_num, top_level in enumerate(available_top_levels):
-    logging_utils.logger.debug(f"({top_level_num+1}/{len(available_top_levels)}) Getting data for top level: '{top_level}'")
+if __name__ == "__main__":
+    # TOP-LEVEL: local, overseas
+    for top_level_num, top_level in enumerate(available_top_levels):
+        logging_utils.logger.debug(f"({top_level_num+1}/{len(available_top_levels)}) Getting data for top level: '{top_level}'")
 
-    cat2_jsons = get_region_er_json(top_level, src_category_code=0)
-    if cat2_jsons == None:
-        logging_utils.logger.warning(f"Unable to get Category 2 JSON data from top_level: {top_level}. Skipping.")
-        continue
-
-    # Write the top-level json file containing category code 2 regions
-    write_json(cat2_jsons,
-            get_output_path([],
-                            f"{top_level}.json"))
-
-    # CATEGORY 2: Local Regions, Overseas Voter Types
-    total_num_regions = len(cat2_jsons['regions'])
-    for cat2_json_num, cat2_json in enumerate(cat2_jsons['regions']):
-        logging_utils.logger.debug(f"({cat2_json_num+1}/{total_num_regions}) Getting data from local region/overseas voter type: '{cat2_json['code']}'({cat2_json['name']})")
-
-        cat3_jsons = get_region_er_json(top_level, src_category_code=2, code=cat2_json['code'])
-
-        if cat3_jsons == None:
-            logging_utils.logger.warning(f"Unable to get Category 3 JSON data from top_level: {top_level}, category 2 code: {cat2_json['code']}({cat2_json['name']}). Skipping.")
+        cat2_jsons = get_region_er_json(top_level, src_category_code=0)
+        if cat2_jsons == None:
+            logging_utils.logger.warning(f"Unable to get Category 2 JSON data from top_level: {top_level}. Skipping.")
             continue
 
-        # Write the category 2 json file containing category code 3 regions
-        write_json(cat3_jsons,
-                get_output_path([
-                                    top_level
-                                ],
-                                f"{cat2_json['code']}.json"))
+        # Write the top-level json file containing category code 2 regions
+        write_json(cat2_jsons,
+                get_output_path([],
+                                f"{top_level}.json"))
 
-        # CATEGORY 3: Local Provinces, Global Regions
-        total_num_provinces = len(cat3_jsons['regions'])
-        for cat3_json_num, cat3_json in enumerate(cat3_jsons['regions']):
-            logging_utils.logger.info(f"({cat3_json_num+1}/{total_num_provinces}) Getting data from province/global region '{cat3_json['code']}'({cat3_json['name']})")
+        # CATEGORY 2: Local Regions, Overseas Voter Types
+        total_num_regions = len(cat2_jsons['regions'])
+        for cat2_json_num, cat2_json in enumerate(cat2_jsons['regions']):
+            logging_utils.logger.debug(f"({cat2_json_num+1}/{total_num_regions}) Getting data from local region/overseas voter type: '{cat2_json['code']}'({cat2_json['name']})")
 
-            cat4_jsons = get_region_er_json(top_level, src_category_code=3, code=cat3_json['code'])
+            cat3_jsons = get_region_er_json(top_level, src_category_code=2, code=cat2_json['code'])
 
-            if cat4_jsons == None:
-                logging_utils.logger.warning(f"Unable to get Category 4 JSON data from top_level: {top_level}, category 2 code: {cat2_json['code']} ({cat2_json['name']}), category 3 code: {cat3_json['code']} ({cat3_json['name']}). Skipping.")
+            if cat3_jsons == None:
+                logging_utils.logger.warning(f"Unable to get Category 3 JSON data from top_level: {top_level}, category 2 code: {cat2_json['code']}({cat2_json['name']}). Skipping.")
                 continue
 
-            # Write the category 3 json file containing category code 4 regions
-            write_json(cat4_jsons,
-                    get_output_path( [
-                                        top_level,
-                                        f"{cat2_json['code']}"
+            # Write the category 2 json file containing category code 3 regions
+            write_json(cat3_jsons,
+                    get_output_path([
+                                        top_level
                                     ],
-                                    f"{cat3_json['code']}.json"))
+                                    f"{cat2_json['code']}.json"))
 
-            # CATEGORY 4: Local Municipalities, Overseas Countries
-            total_num_municipalities = len(cat4_jsons['regions'])
-            for cat4_json_num, cat4_json in enumerate(cat4_jsons['regions']):
-                logging_utils.logger.debug(f"({cat4_json_num+1}/{total_num_municipalities}) Getting data from municipality/country: '{cat4_json['code']}'({cat4_json['name']})")
+            # CATEGORY 3: Local Provinces, Global Regions
+            total_num_provinces = len(cat3_jsons['regions'])
+            for cat3_json_num, cat3_json in enumerate(cat3_jsons['regions']):
+                logging_utils.logger.info(f"({cat3_json_num+1}/{total_num_provinces}) Getting data from province/global region '{cat3_json['code']}'({cat3_json['name']})")
 
-                cat5_jsons = get_region_er_json(top_level, src_category_code=4, code=cat4_json['code'])
+                cat4_jsons = get_region_er_json(top_level, src_category_code=3, code=cat3_json['code'])
 
-                if cat5_jsons == None:
-                    logging_utils.logger.warning(f"Unable to get Category 5 JSON data from top_level: {top_level}, category 2 code: {cat2_json['code']} ({cat2_json['name']}), category 3 code: {cat3_json['code']} ({cat3_json['name']}), category 4 code: {cat4_json['code']} ({cat4_json['name']}). Skipping.")
+                if cat4_jsons == None:
+                    logging_utils.logger.warning(f"Unable to get Category 4 JSON data from top_level: {top_level}, category 2 code: {cat2_json['code']} ({cat2_json['name']}), category 3 code: {cat3_json['code']} ({cat3_json['name']}). Skipping.")
                     continue
 
-                # Write the category 4 json file containing category code 5 regions
-                write_json(cat5_jsons,
+                # Write the category 3 json file containing category code 4 regions
+                write_json(cat4_jsons,
                         get_output_path( [
                                             top_level,
-                                            f"{cat2_json['code']}",
-                                            f"{cat3_json['code']}"
+                                            f"{cat2_json['code']}"
                                         ],
-                                        f"{cat4_json['code']}.json"))
+                                        f"{cat3_json['code']}.json"))
 
-                # CATEGORY 5: Local Baranggays, Overseas Jurisdictions
-                total_num_brgys = len(cat5_jsons['regions'])
-                for cat5_json_num, cat5_json in enumerate(cat5_jsons['regions']):
-                    logging_utils.logger.debug(f"({cat5_json_num+1}/{total_num_brgys}) Getting data from baranggay/jurisdiction: '{cat5_json['code']}'({cat5_json['name']})")
+                # CATEGORY 4: Local Municipalities, Overseas Countries
+                total_num_municipalities = len(cat4_jsons['regions'])
+                for cat4_json_num, cat4_json in enumerate(cat4_jsons['regions']):
+                    logging_utils.logger.debug(f"({cat4_json_num+1}/{total_num_municipalities}) Getting data from municipality/country: '{cat4_json['code']}'({cat4_json['name']})")
 
-                    precinct_jsons = get_region_er_json(top_level, src_category_code=5, code=cat5_json['code'])
+                    cat5_jsons = get_region_er_json(top_level, src_category_code=4, code=cat4_json['code'])
 
-                    if precinct_jsons == None:
-                        logging_utils.logger.warning(f"Unable to get precinct JSON data from top_level: {top_level}, category 2 code: {cat2_json['code']} ({cat2_json['name']}), category 3 code: {cat3_json['code']} ({cat3_json['name']}), category 4 code: {cat4_json['code']} ({cat4_json['name']}), category 5 code: {cat5_json['code']} ({cat5_json['name']}). Skipping.")
+                    if cat5_jsons == None:
+                        logging_utils.logger.warning(f"Unable to get Category 5 JSON data from top_level: {top_level}, category 2 code: {cat2_json['code']} ({cat2_json['name']}), category 3 code: {cat3_json['code']} ({cat3_json['name']}), category 4 code: {cat4_json['code']} ({cat4_json['name']}). Skipping.")
                         continue
 
-                    # CATEGORY NULL: Precincts
-                    for precinct_json in precinct_jsons['regions']:
-                        logging_utils.logger.info(f" Getting election results from "\
-                                                f"top level: '{top_level}', "\
-                                                f"cat2 code: '{cat2_json['code']}'({cat2_json['name']}), "\
-                                                f"cat3 code: '{cat3_json['code']}'({cat3_json['name']}), "\
-                                                f"cat4 code: '{cat4_json['code']}'({cat4_json['name']}), "\
-                                                f"cat5 code: '{cat5_json['code']}'({cat5_json['name']}), "\
-                                                f"precinct code: '{precinct_json['code']}'")
+                    # Write the category 4 json file containing category code 5 regions
+                    write_json(cat5_jsons,
+                            get_output_path( [
+                                                top_level,
+                                                f"{cat2_json['code']}",
+                                                f"{cat3_json['code']}"
+                                            ],
+                                            f"{cat4_json['code']}.json"))
 
-                        er_jsons = get_region_er_json(top_level, src_category_code=None, code=precinct_json['code'])
+                    # CATEGORY 5: Local Baranggays, Overseas Jurisdictions
+                    total_num_brgys = len(cat5_jsons['regions'])
+                    for cat5_json_num, cat5_json in enumerate(cat5_jsons['regions']):
+                        logging_utils.logger.debug(f"({cat5_json_num+1}/{total_num_brgys}) Getting data from baranggay/jurisdiction: '{cat5_json['code']}'({cat5_json['name']})")
 
-                        if er_jsons == None:
-                            logging_utils.logger.warning(f"Unable to get election results JSON data from top_level: {top_level}, category 2 code: {cat2_json['code']} ({cat2_json['name']}), category 3 code: {cat3_json['code']} ({cat3_json['name']}), category 4 code: {cat4_json['code']} ({cat4_json['name']}), category 5 code: {cat5_json['code']} ({cat5_json['name']}), precinct code: {precinct_json['code']}. Skipping.")
+                        precinct_jsons = get_region_er_json(top_level, src_category_code=5, code=cat5_json['code'])
+
+                        if precinct_jsons == None:
+                            logging_utils.logger.warning(f"Unable to get precinct JSON data from top_level: {top_level}, category 2 code: {cat2_json['code']} ({cat2_json['name']}), category 3 code: {cat3_json['code']} ({cat3_json['name']}), category 4 code: {cat4_json['code']} ({cat4_json['name']}), category 5 code: {cat5_json['code']} ({cat5_json['name']}). Skipping.")
                             continue
 
-                        # Write the category 5 json file containing precinct data
-                        write_json(er_jsons,
-                                get_output_path( [
-                                                    top_level,
-                                                    f"{cat2_json['code']}",
-                                                    f"{cat3_json['code']}",
-                                                    f"{cat4_json['code']}",
-                                                    f"{cat5_json['code']}"
-                                                ],
-                                                f"{precinct_json['code']}.json"))
+                        # CATEGORY NULL: Precincts
+                        for precinct_json in precinct_jsons['regions']:
+                            logging_utils.logger.info(f" Getting election results from "\
+                                                    f"top level: '{top_level}', "\
+                                                    f"cat2 code: '{cat2_json['code']}'({cat2_json['name']}), "\
+                                                    f"cat3 code: '{cat3_json['code']}'({cat3_json['name']}), "\
+                                                    f"cat4 code: '{cat4_json['code']}'({cat4_json['name']}), "\
+                                                    f"cat5 code: '{cat5_json['code']}'({cat5_json['name']}), "\
+                                                    f"precinct code: '{precinct_json['code']}'")
 
-logging_utils.logger.info("PH Election Results 2025 Scraper: Ended.")
+                            er_jsons = get_region_er_json(top_level, src_category_code=None, code=precinct_json['code'])
+
+                            if er_jsons == None:
+                                logging_utils.logger.warning(f"Unable to get election results JSON data from top_level: {top_level}, category 2 code: {cat2_json['code']} ({cat2_json['name']}), category 3 code: {cat3_json['code']} ({cat3_json['name']}), category 4 code: {cat4_json['code']} ({cat4_json['name']}), category 5 code: {cat5_json['code']} ({cat5_json['name']}), precinct code: {precinct_json['code']}. Skipping.")
+                                continue
+
+                            # Write the category 5 json file containing precinct data
+                            write_json(er_jsons,
+                                    get_output_path( [
+                                                        top_level,
+                                                        f"{cat2_json['code']}",
+                                                        f"{cat3_json['code']}",
+                                                        f"{cat4_json['code']}",
+                                                        f"{cat5_json['code']}"
+                                                    ],
+                                                    f"{precinct_json['code']}.json"))
+
+    logging_utils.logger.info("PH Election Results 2025 Scraper: Ended.")
